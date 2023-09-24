94c1c2bf48712b0760db5a5f3b528ed9e3a93730
Fin157 <444stuart@gmail.com>
2023-09-24 12:33:05 +0200

    Laid out the basic code structure.

---
+++ main.py
@@ -0,0 +1,6 @@
+def go_home(pub_x, pub_y, step_size, max_steps):
+    # Initialization
+
+    # Walk until any ending point (either home or pub) is hit or until the max steps limit is reached
+
+    # Inform the user of the result of this simulation run
